e54abf9ef677fa0c8c8771bc058445c06648d326
Ivan <info@ivansmiths.com>
2022-06-23 10:32:19 +0100

    fix homepage

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,11 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import db from "../utils/db";
 import Product from "../models/Product";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useContext } from "react";
-import { Store } from "../utils/Store";
 import ProductItem from "../components/ProductItem";
-import useStyles from "../utils/styles";
 
 export default function Home(props) {
-  const classes = useStyles();
-  const router = useRouter();
-  const { state, dispatch } = useContext(Store);
   const { topRatedProducts } = props;
-  const addToCartHandler = async (product) => {
-    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
-    if (data.countInStock < quantity) {
-      window.alert("Sorry. Product is out of stock");
-      return;
-    }
-    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
-    router.push("/cart");
-  };
+
   return (
     <Layout>
       <div container spacing={3}>
@@ -33,10 +15,7 @@ export default function Home(props) {
           <Link href={`product/${product.slug}`} key={product.name}>
             <a>
               <div item md={4}>
-                <ProductItem
-                  product={product}
-                  addToCartHandler={addToCartHandler}
-                />
+                <ProductItem product={product} />
               </div>
             </a>
           </Link>
