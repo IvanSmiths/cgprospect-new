95edb65ff9f5875bf223068b1680bd1f62bb701c
Ivan <info@ivansmiths.com>
2022-06-27 18:10:00 +0100

    p0

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head></Head>
       <Navbar />
       <Component {...pageProps}></Component>
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,72 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/images/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="application-name" content="CG Prospect" />
+          <meta content="#ffda38" name="theme-color" />
+          <meta name="apple-mobile-web-app-title" content="CG Prospect" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/images/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/images/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/images/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/images/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/images/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/images/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
+          <meta
+            name="p:domain_verify"
+            content="fbf05c0e98334a043794496750861ac3"
+          />
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta name="msapplication-TileColor" content="#ffda38" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="msapplication-navbutton-color" content="#ffda38" />
+        </Head>
         <body>
           <Main />
           <NextScript />
